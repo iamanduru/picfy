ee8a5c2ab6aea5d0980fa8fff27714fd9ba8aa9b
Mitchelle <104675426+iamanduru@users.noreply.github.com>
2024-04-03 14:59:24 +0200

    Still building

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//It will run it is own id by using dynamic routes, that'ts wrapping with Square brackets'
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPLEX = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"], 
   weight: ['400', '500', '600', '700'],
   variable: '__font-ibm-plex',
@@ -20,9 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}</body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
